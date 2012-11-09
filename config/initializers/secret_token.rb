8c1d1beccf228d3886fd4b828dd401b7d24fe266
Vicente Leyba <leyba182@unm.edu>
2012-11-09 15:05:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JewelryStore::Application.config.secret_token = 'ca5609bd9d8c8569ed9b2b2ae16ce33873d8d9362e4be5354ea74bda656433d765a1c0b00b1e43dfa220d3d5f618613eb3270d87a0562ce99809ed5c6acb11f8'
